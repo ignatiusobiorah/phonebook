62a47d238832adcf90720a404bba50d53c2ad343
ignatiusobiorah <iobiorah23@gmail.com>
2022-08-04 14:43:34 +0100

    updated input to make use of one useState for both name and phone

--- src/App.js
+++ src/App.js
@@ -1,106 +1,132 @@
-import './index.scss';
-import afrodroid from './img/afrodroid.jpg';
+import "./index.scss";
+import afrodroid from "./img/afrodroid.jpg";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { useState } from 'react';
-
-
+import { useState } from "react";
 
 const App = () => {
-
   //useStates
   const [view, setView] = useState(true);
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  // const [name, setName] = useState("");
+  // const [phone, setPhone] = useState("");
+  const [input, setInput] = useState({ name: "", phone: "" });
   const [contacts, setContacts] = useState([]);
 
   //Function to handle submit
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    const newContact = [
-      {
-        nameInput: name,
-        phoneInput: phone,
-        id: contacts.length + 1
-      }, ...contacts
-    ]
+    if (input.name == "" || input.phone == "") return;
+
+    const newContact = [{ ...input, id: contacts.length + 1 }, ...contacts];
+
     setContacts(newContact);
-    //  console.log(name);
-    // console.log(phone);
-    setName('');
-    setPhone('');
+    setInput({
+      name: "",
+      phone: "",
+    });
     setView(!view);
   }
 
   //Function to delete contact
   const deleteContact = (e) => {
-    const retrivedId = Number (e.target.getAttribute("id"));
-   const filteredContacts = contacts.filter(contact => 
-      contact.id !== retrivedId
-   )
-    setContacts(filteredContacts);
+    const retrivedId = Number(e.target.getAttribute("id"));
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== retrivedId
+    );
+
     // console.log(retrivedId);
     // console.log(filteredContacts);
-  }
+    setContacts(filteredContacts);
+  };
 
-  //Function to toggle dark mode 
+  //Function to toggle dark mode
   const toggleDarkMode = () => {
     const body = document.querySelector("body");
     body.classList.toggle("dark");
   };
 
- 
-
-
   return (
     <div className="container">
       <nav>
-        <a href="/"><img src={afrodroid} alt="logo" /></a>
+        <a href="/">
+          <img src={afrodroid} alt="logo" />
+        </a>
         <i onClick={toggleDarkMode} className="fa-solid fa-moon"></i>
       </nav>
-      <div className='main'>
+      <div className="main">
         <form>
-          <input type="search" placeholder='Search contacts' />
+          <input type="search" placeholder="Search contacts" />
         </form>
-        <div className='list'>
+        <div className="list">
           <h2>Contact List</h2>
-          <button onClick={ () => setView(!view)}>{view ? "Add Contact" : "View Contact"}
+          <button onClick={() => setView(!view)}>
+            {view ? "Add Contact" : "View Contact"}
           </button>
         </div>
-        {view ?  <div className='table'>
-          <table>
-            {contacts.length > 0 ? 
-            <thead>
-            <tr>
-              <th>NAME</th>
-              <th>PHONE</th>
-              <th>ACTION</th>
-            </tr>
-          </thead> : <h2>No contacts found</h2>}
-            <tbody>
-              {contacts.map(contact => (
-                <tr key={contact.id}>
-                <td>{contact.nameInput}</td>
-                <td>{contact.phoneInput}</td>
-                <td><i className="fa-regular fa-pen-to-square" id={contact.id}></i> <i className="fa-solid fa-trash" id={contact.id} onClick={deleteContact}></i> </td>
-              </tr>
-              ))}
-              
-            </tbody>
-          </table>
-        </div> : 
-          <div className='newContact'>
-          <h2>Add Contact</h2>
-            <form className='addContact'>
-            <label htmlFor="name">NAME</label>
-            <input type="text" placeholder='Full name' value={name} onChange = {(e) => setName(e.target.value) } />
-            <label htmlFor="phone">PHONE NUMBER:</label>
-            <input type="number" placeholder='phoneNumber' value={phone} onChange = {(e) => setPhone(e.target.value)} />
-            <button className='submit' onClick={handleSubmit}> submit</button>
-          </form>      
-        </div>}
-     
-        
+        {view ? (
+          <div className="table">
+            <table>
+              {contacts.length > 0 ? (
+                <thead>
+                  <tr>
+                    <th>NAME</th>
+                    <th>PHONE</th>
+                    <th>ACTION</th>
+                  </tr>
+                </thead>
+              ) : (
+                <thead>
+                  <tr>
+                    <th>No contacts found</th>
+                  </tr>
+                </thead>
+              )}
+              <tbody>
+                {contacts.map((contact) => (
+                  <tr key={contact.id}>
+                    <td>{contact.name}</td>
+                    <td>{contact.phone}</td>
+                    <td>
+                      <i className="fa-regular fa-pen-to-square"></i>{" "}
+                      <i
+                        className="fa-solid fa-trash"
+                        id={contact.id}
+                        onClick={deleteContact}
+                      ></i>{" "}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="newContact">
+            <h2>Add Contact</h2>
+            <form className="addContact">
+              <label htmlFor="name">NAME</label>
+              <input
+                type="text"
+                placeholder="Full name"
+                value={input.name}
+                onChange={(e) =>
+                  setInput({ name: e.target.value, phone: input.phone })
+                }
+              />
+              <label htmlFor="phone">PHONE NUMBER:</label>
+              <input
+                type="number"
+                placeholder="phoneNumber"
+                value={input.phone}
+                onChange={(e) =>
+                  setInput({ name: input.name, phone: e.target.value })
+                }
+              />
+              <button className="submit" onClick={handleSubmit}>
+                submit
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -108,4 +134,6 @@ const App = () => {
 
 export default App;
 
-//Still yet to write code to search contacts
+//Haven't added functionality to some places yet
+//-- search bar
+//-- edit icon
